903648c3d229bc3f1141c020e9c30c7d0a90624b
Igor Oliveira <i.oliveira@email.com>
2024-03-27 15:00:41 -0300

    Base project created

---
+++ noter-backend/src/test/java/dev/ioliver/noterbackend/NoterBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ioliver.noterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
