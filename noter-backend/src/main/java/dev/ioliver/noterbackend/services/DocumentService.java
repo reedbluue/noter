bfb80088117a465bbda036a569276b55783263e9
Igor Oliveira <i.oliveira@email.com>
2024-03-28 13:23:58 -0300

    added the update document event

--- noter-backend/src/main/java/dev/ioliver/noterbackend/services/DocumentService.java
+++ noter-backend/src/main/java/dev/ioliver/noterbackend/services/DocumentService.java
@@ -3,12 +3,15 @@ package dev.ioliver.noterbackend.services;
 import dev.ioliver.noterbackend.domain.entities.Document;
 import dev.ioliver.noterbackend.dtos.request.document.DocumentUpdateDTO;
 import dev.ioliver.noterbackend.dtos.response.document.DocumentDTO;
+import dev.ioliver.noterbackend.dtos.response.document.DocumentUpdatedEventDTO;
+import dev.ioliver.noterbackend.events.document.DocumentUpdatedEvent;
 import dev.ioliver.noterbackend.exceptions.document.DocumentNotFoundException;
 import dev.ioliver.noterbackend.mappers.DocumentMapper;
 import dev.ioliver.noterbackend.repositories.DocumentRepository;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +22,7 @@ import org.springframework.validation.annotation.Validated;
 public class DocumentService {
   private final DocumentRepository documentRepository;
   private final DocumentMapper documentMapper;
+  private final ApplicationEventPublisher applicationEventPublisher;
 
   @Transactional(readOnly = true)
   public DocumentDTO get(@NotNull UUID id) throws DocumentNotFoundException {
@@ -41,6 +45,8 @@ public class DocumentService {
     Document document = documentMapper.toEntity(get(documentUpdateDTO.id()));
     documentMapper.update(documentUpdateDTO, document);
     Document saved = documentRepository.save(document);
+    applicationEventPublisher.publishEvent(
+        new DocumentUpdatedEvent(this, new DocumentUpdatedEventDTO(saved.getId())));
     return documentMapper.toDto(saved);
   }
 }
